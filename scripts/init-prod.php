dbfa98dd3b6adacc6eb416ef4165de9eaef1a5b1
Maciej Klemarczyk <m.klemarczyk@live.com>
2017-08-10 18:44:54 +0200

    Empty advanced template Yii 2.0.12 was created.

---
+++ scripts/init-prod.php
@@ -0,0 +1,14 @@
+<?php
+
+echo('<pre>');
+
+if(!isset($_SERVER['argv']) || !is_array($_SERVER['argv']) || count($_SERVER['argv']) === 0){
+	$_SERVER['argv'] = [$_SERVER['SCRIPT_NAME']];
+}
+
+$_SERVER['argv'][] = '--env=Production';
+$_SERVER['argv'][] = '--overwrite=y';
+
+include('init');
+
+echo('</pre>');
