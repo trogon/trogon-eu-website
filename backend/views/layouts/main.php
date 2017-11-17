cc369e720018fe23e364aa1e799a83a1df4884ae
Maciej Klemarczyk <m.klemarczyk@live.com>
2017-11-17 20:57:31 +0100

    [Backend] Privacy terms were included in menu.

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -37,6 +37,7 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'Privacy terms', 'url' => ['/privacy-terms/index']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
