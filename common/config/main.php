5b8e00cc557916464ac6257ae5dfd1199e6dcd64
Maciej Klemarczyk <m.klemarczyk@live.com>
2017-11-17 22:29:29 +0100

    [Common] Redactor and Storage modules configuration was applied in common configuration.

--- common/config/main.php
+++ common/config/main.php
@@ -6,4 +6,18 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+    'modules' => [
+		'storage' => [
+			'class' => 'insma\storage\StorageModule',
+			'localStorageDir' => '@common/data/uploads',
+			'webAccessUrl' => false,
+		],
+        'redactor' => [
+			'class' => 'yii\redactor\RedactorModule',
+			'imageUploadRoute' => ['storage/image/upload'],
+			'fileUploadRoute' => ['storage/file/upload'],
+			'imageManagerJsonRoute' => ['storage/image/json-index'],
+			'fileManagerJsonRoute' => ['storage/file/json-index'],
+		],
+    ],
 ];
