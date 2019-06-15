49fedee70698c4951e5d17e7c83121795e1f24a2
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2019-06-15 22:44:38 +0200

    Removed Yii framework with website code Prepared for symfony introduction

--- environments/dev/common/config/params-local.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'companyName' => 'My Application',
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
-    'user.passwordResetTokenExpire' => 3600,
-];
