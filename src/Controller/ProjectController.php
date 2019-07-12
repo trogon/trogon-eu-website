f06dfda4bc4c9770f40d5932f68966135b14b216
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2019-07-12 22:55:49 +0200

    ProjectController adjusted to use database information instead query third-party services each time

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -7,6 +7,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Service\LayoutService;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpClient\Exception\ClientException;
+use Symfony\Component\HttpClient\Exception\TransportException;
+
+use App\Entity\Project;
 
 class ProjectController extends AbstractController
 {
@@ -14,45 +17,24 @@ class ProjectController extends AbstractController
      * @Route("/projects", methods={"GET"})
      */
     public function list(
-        LayoutService $layout,
-        HttpClientInterface $githubApiClient,
-        HttpClientInterface $bitbucketApiClient)
+        LayoutService $layout)
     {
         $layout->breadcrumbs[] = [
             'label' => 'Projects',
             'route' => 'app_project_list'
         ];
 
-        $repos = [];
+        $showPrivate = false;
+        $projects = $this->getDoctrine()
+            ->getRepository(Project::class)
+            ->findBy(['is_private' => $showPrivate]);
 
-        try {
-            $bbRepos = $bitbucketApiClient->request('GET', '/2.0/repositories/trogon-studios');
-            foreach ($bbRepos->toArray()['values'] as $project) {
-                $repos[] = [
-                    'name' => $project['name'],
-                    'url' => "https://www.bitbucket.com/{$project['full_name']}",
-                    'description' => $project['description']
-                ];
-            }
-        } catch (ClientException $ex) { }
-
-        try {
-            $ghRepos = $githubApiClient->request('GET', '/users/trogon/repos');
-            foreach ($ghRepos->toArray() as $project) {
-                $repos[] = [
-                    'name' => $project['name'],
-                    'url' => "https://www.github.com/{$project['full_name']}",
-                    'description' => $project['description']
-                ];
-            }
-        } catch (ClientException $ex) { }
-
-        usort($repos, function ($a, $b) {
-            return strcmp($a['name'], $b['name']);
+        usort($projects, function ($a, $b) {
+            return strcasecmp($a->getName(), $b->getName());
         });
 
         return $this->render('project/list.html.twig', [
-            'repos' => $repos,
+            'projects' => $projects,
         ]);
     }
 }
