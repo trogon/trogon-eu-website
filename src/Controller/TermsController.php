5c10df264020e996014cfec122c2773d737a975c
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2020-08-17 16:34:13 +0200

    Basic draft of privacy policy

---
+++ src/Controller/TermsController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Service\LayoutService;
+
+use App\Entity\Project;
+
+class TermsController extends AbstractController
+{
+    /**
+     * @Route("/privacy", methods={"GET"})
+     */
+    public function privacy(
+        LayoutService $layout)
+    {
+        $layout->breadcrumbs[] = [
+            'label' => 'Privacy',
+            'route' => 'app_terms_privacy'
+        ];
+
+        return $this->render('terms/privacy.html.twig');
+    }
+}
