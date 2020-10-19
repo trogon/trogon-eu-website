4447231e40c44f6ecd9d2bf9c13c64012f5cf9ad
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2020-10-19 18:12:24 +0200

    Install bootstrap, jQuery and popper.js libraries as direct dependency so application scripts can use it with Node JS.

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,17 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+// assets/app.js
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+// console.log('Hello Webpack Encore! Edit me in assets/app.js');
