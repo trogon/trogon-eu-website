94228283785dda527c3b2db36ec1ebe924323038
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2020-10-28 18:44:29 +0100

    Store webhooks payloads in the logs for further analise.

---
+++ src/Controller/WebhookController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WebhookController extends AbstractController
+{
+    /**
+     * @Route("/api/webhook", methods={"POST"})
+     */
+    public function receivePayload(Request $request, LoggerInterface $logger)
+    {
+        $logger->info($request->getContent(), ["source" => "webhook", "format" => "json"]);
+
+        return $this->json([
+            "status" => "OK",
+        ]);
+    }
+}
